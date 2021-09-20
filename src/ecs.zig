21bdff366e06fcef89d1fea3705e9d2867452692
Adam Kowalski <adam.kowalski.work@gmail.com>
2021-09-19 17:39:18 -0700

    query with tuple

--- src/ecs.zig
+++ src/ecs.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const assert = std.debug.assert;
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
+const expectEqualStrings = std.testing.expectEqualStrings;
+const expectEqualSlices = std.testing.expectEqualSlices;
+const TypeInfo = std.builtin.TypeInfo;
 
 fn Component(comptime T: type) type {
     return struct {
@@ -85,31 +89,59 @@ const ECS = struct {
         self.components.deinit();
     }
 
-    fn new(self: *ECS) Entity {
+    fn create_entity(self: *ECS, components: anytype) !Entity {
         const uuid = self.next_uuid;
         self.next_uuid += 1;
-        return Entity{
+        const entity = Entity{
             .uuid = uuid,
             .ecs = self,
         };
+        return try entity.set(components);
     }
 };
+
+fn Query(comptime components: anytype) type {
+    const components_fields = @typeInfo(@TypeOf(components)).Struct.fields;
+    var fields: [components_fields.len]TypeInfo.StructField = undefined;
+    inline for (components_fields) |field, i| {
+        const T = @field(components, field.name);
+        fields[i] = TypeInfo.StructField{
+            .name = @typeName(T),
+            .field_type = *const T,
+            .default_value = null,
+            .is_comptime = false,
+            .alignment = 8,
+        };
+    }
+    return @Type(TypeInfo{ .Struct = .{
+        .layout = TypeInfo.ContainerLayout.Auto,
+        .fields = &fields,
+        .decls = &[_]TypeInfo.Declaration{},
+        .is_tuple = false,
+    } });
+}
 
 const Entity = struct {
     uuid: u64,
     ecs: *ECS,
 
-    fn set(self: Entity, value: anytype) !void {
-        const T = @TypeOf(value);
-        const result = try self.ecs.components.getOrPut(@typeName(T));
-        if (result.found_existing) {
-            try @intToPtr(*Component(T), result.value_ptr.*).*.set(self, value);
-        } else {
-            const component = try self.ecs.allocator.create(Component(T));
-            component.* = Component(T).init(self.ecs.allocator);
-            try component.*.set(self, value);
-            result.value_ptr.* = @ptrToInt(component);
+    fn set(self: Entity, components: anytype) !Entity {
+        const type_info = @typeInfo(@TypeOf(components)).Struct;
+        assert(type_info.is_tuple);
+        inline for (type_info.fields) |field| {
+            const T = field.field_type;
+            const result = try self.ecs.components.getOrPut(@typeName(T));
+            if (result.found_existing) {
+                const component = @intToPtr(*Component(T), result.value_ptr.*);
+                try component.*.set(self, @field(components, field.name));
+            } else {
+                const component = try self.ecs.allocator.create(Component(T));
+                component.* = Component(T).init(self.ecs.allocator);
+                try component.*.set(self, @field(components, field.name));
+                result.value_ptr.* = @ptrToInt(component);
+            }
         }
+        return self;
     }
 
     fn get(self: Entity, comptime T: type) ?*const T {
@@ -119,6 +151,32 @@ const Entity = struct {
             return null;
         }
     }
+
+    fn query(entity: Entity, comptime components: anytype) ?Query(components) {
+        var result: Query(components) = undefined;
+        var found = true;
+        inline for (@typeInfo(@TypeOf(components)).Struct.fields) |field| {
+            const T = @field(components, field.name);
+            if (entity.get(T)) |component| {
+                @field(result, @typeName(T)) = component;
+            } else {
+                found = false;
+            }
+        }
+        if (!found) {
+            return null;
+        } else {
+            return result;
+        }
+    }
+};
+
+const Name = struct {
+    value: []const u8,
+};
+
+const Age = struct {
+    value: u8,
 };
 
 test "entity get and set component" {
@@ -127,10 +185,71 @@ test "entity get and set component" {
     const allocator = &gpa.allocator;
     var ecs = ECS.init(allocator);
     defer ecs.deinit();
-    const entity = ecs.new();
-    try expectEqual(entity.get(u64), null);
-    try entity.set(@as(u64, 10));
-    try expectEqual(entity.get(u64).?.*, 10);
-    try entity.set(@as(u64, 20));
-    try expectEqual(entity.get(u64).?.*, 20);
+    const entity = try ecs.create_entity(.{});
+    try expectEqual(entity.get(Name), null);
+    _ = try entity.set(.{Name{ .value = "Joe" }});
+    try expectEqual(entity.get(Name).?.*, Name{ .value = "Joe" });
+    _ = try entity.set(.{Name{ .value = "Bob" }});
+    try expectEqual(entity.get(Name).?.*, Name{ .value = "Bob" });
+}
+
+test "entity get and set components" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer expect(!gpa.deinit()) catch @panic("MEMORY LEAK");
+    const allocator = &gpa.allocator;
+    var ecs = ECS.init(allocator);
+    defer ecs.deinit();
+    const entity = try ecs.create_entity(.{});
+    _ = try entity.set(.{ Name{ .value = "Joe" }, Age{ .value = 20 } });
+    try expectEqual(entity.get(Name).?.*, Name{ .value = "Joe" });
+    try expectEqual(entity.get(Age).?.*, Age{ .value = 20 });
+}
+
+test "entity get and set components on creation" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer expect(!gpa.deinit()) catch @panic("MEMORY LEAK");
+    const allocator = &gpa.allocator;
+    var ecs = ECS.init(allocator);
+    defer ecs.deinit();
+    const entity = try ecs.create_entity(.{ Name{ .value = "Joe" }, Age{ .value = 20 } });
+    try expectEqual(entity.get(Name).?.*, Name{ .value = "Joe" });
+    try expectEqual(entity.get(Age).?.*, Age{ .value = 20 });
+}
+
+test "query" {
+    const type_info = @typeInfo(Query(.{ Name, Age })).Struct;
+    try expectEqual(type_info.layout, TypeInfo.ContainerLayout.Auto);
+    try expectEqual(type_info.fields.len, 2);
+    try expectEqual(type_info.fields[0], .{
+        .name = "Name",
+        .field_type = *const Name,
+        .default_value = null,
+        .is_comptime = false,
+        .alignment = 8,
+    });
+    try expectEqual(type_info.fields[1], .{
+        .name = "Age",
+        .field_type = *const Age,
+        .default_value = null,
+        .is_comptime = false,
+        .alignment = 8,
+    });
+    try expectEqualSlices(TypeInfo.Declaration, type_info.decls, &[_]TypeInfo.Declaration{});
+    try expectEqual(type_info.is_tuple, false);
+}
+
+test "entity query components" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer expect(!gpa.deinit()) catch @panic("MEMORY LEAK");
+    const allocator = &gpa.allocator;
+    var ecs = ECS.init(allocator);
+    defer ecs.deinit();
+    const entity = try ecs.create_entity(.{});
+    try expectEqual(entity.query(.{ Name, Age }), null);
+    _ = try entity.set(.{Name{ .value = "Joe" }});
+    try expectEqual(entity.query(.{ Name, Age }), null);
+    _ = try entity.set(.{Age{ .value = 20 }});
+    const query = entity.query(.{ Name, Age }).?;
+    try expectEqual(query.Name.*, Name{ .value = "Joe" });
+    try expectEqual(query.Age.*, Age{ .value = 20 });
 }
