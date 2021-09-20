0aaec4c5f09a6c18f894bbdd53148afd9bdf9354
Adam Kowalski <adam.kowalski.work@gmail.com>
2021-09-19 22:07:48 -0700

    intern strings

---
+++ src/strings.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
+const eql = std.meta.eql;
+
+const InternedString = struct {
+    value: u64,
+};
+
+const Strings = struct {
+    lookup: std.StringHashMap(InternedString),
+    next: InternedString,
+    allocator: *Allocator,
+
+    fn init(allocator: *Allocator) Strings {
+        return Strings{
+            .lookup = std.StringHashMap(InternedString).init(allocator),
+            .next = InternedString{ .value = 0 },
+            .allocator = allocator,
+        };
+    }
+
+    fn deinit(self: *Strings) void {
+        self.lookup.deinit();
+    }
+
+    fn intern(self: *Strings, value: []const u8) !InternedString {
+        const result = try self.lookup.getOrPut(value);
+        if (result.found_existing) {
+            return result.value_ptr.*;
+        } else {
+            const interned = self.next;
+            result.value_ptr.* = interned;
+            self.next.value += 1;
+            return interned;
+        }
+    }
+};
+
+test "intern a string" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer expect(!gpa.deinit()) catch @panic("MEMORY LEAK");
+    const allocator = &gpa.allocator;
+    var strings = Strings.init(allocator);
+    defer strings.deinit();
+    const joe = try strings.intern("Joe");
+    const bob = try strings.intern("Bob");
+    try expect(!eql(joe, bob));
+    const joe_again = try strings.intern("Joe");
+    try expectEqual(joe, joe_again);
+}
