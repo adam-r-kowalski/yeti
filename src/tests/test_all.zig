6d8fdb16ab9126067400c6f12fb44ba5fa53a297
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-02-20 16:30:36 -0800

    import unit tests extracted

--- src/tests/test_all.zig
+++ src/tests/test_all.zig
@@ -1,11 +1,6 @@
 const std = @import("std");
 
-const test_tokenizer = @import("test_tokenizer.zig");
-const test_parser = @import("test_parser.zig");
-const test_semantic_analyzer = @import("test_semantic_analyzer.zig");
 const test_uniform_function_call_syntax = @import("test_uniform_function_call_syntax.zig");
-const test_codegen = @import("test_codegen.zig");
-const test_wasm_printer = @import("test_wasm_printer.zig");
 const test_error_printer = @import("test_error_printer.zig");
 const test_ecs = @import("test_ecs.zig");
 const test_list = @import("test_list.zig");
@@ -26,6 +21,7 @@ const test_binary_op = @import("test_binary_op.zig");
 const test_define = @import("test_define.zig");
 const test_function = @import("test_function.zig");
 const test_primitive = @import("test_primitive.zig");
+const test_import = @import("test_import.zig");
 
 test "run all tests" {
     std.testing.refAllDecls(@This());
--- src/tests/test_import.zig
+++ src/tests/test_import.zig
@@ -5,14 +5,55 @@ const expectEqualStrings = std.testing.expectEqualStrings;
 
 const yeti = @import("yeti");
 const initCodebase = yeti.initCodebase;
-const MockFileSystem = yeti.FileSystem;
-const components = yeti.components;
+const tokenize = yeti.tokenize;
+const parse = yeti.parse;
 const analyzeSemantics = yeti.analyzeSemantics;
+const codegen = yeti.codegen;
+const printWasm = yeti.printWasm;
+const components = yeti.components;
 const literalOf = yeti.test_utils.literalOf;
 const typeOf = yeti.test_utils.typeOf;
-const parentType = yeti.test_utils.parentType;
-const valueType = yeti.test_utils.valueType;
+const MockFileSystem = yeti.FileSystem;
 const Entity = yeti.ecs.Entity;
+
+test "parse import and function" {
+    var arena = Arena.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var codebase = try initCodebase(&arena);
+    const module = try codebase.createEntity(.{});
+    const code =
+        \\math = import("math.yeti")
+        \\
+        \\start(): u64 {
+        \\  math.sum_of_squares(10, 56 * 3)
+        \\}
+    ;
+    var tokens = try tokenize(module, code);
+    try parse(module, &tokens);
+    const top_level = module.get(components.TopLevel);
+    const math = top_level.findString("math");
+    try expectEqual(math.get(components.AstKind), .import);
+    try expectEqualStrings(literalOf(math.get(components.Path).entity), "math.yeti");
+    const start = top_level.findString("start");
+    const overloads = start.get(components.Overloads).slice();
+    try expectEqual(overloads.len, 1);
+    const body = overloads[0].get(components.Body).slice();
+    try expectEqual(body.len, 1);
+    const dot = body[0];
+    try expectEqual(dot.get(components.AstKind), .binary_op);
+    const dot_arguments = dot.get(components.Arguments).slice();
+    try expectEqualStrings(literalOf(dot_arguments[0]), "math");
+    const sum_of_squares = dot_arguments[1];
+    const callable = sum_of_squares.get(components.Callable).entity;
+    try expectEqualStrings(literalOf(callable), "sum_of_squares");
+    const sum_of_squares_arguments = sum_of_squares.get(components.Arguments).slice();
+    try expectEqualStrings(literalOf(sum_of_squares_arguments[0]), "10");
+    const multiply = sum_of_squares_arguments[1];
+    try expectEqual(multiply.get(components.BinaryOp), .multiply);
+    const arguments = multiply.get(components.Arguments).slice();
+    try expectEqualStrings(literalOf(arguments[0]), "56");
+    try expectEqualStrings(literalOf(arguments[1]), "3");
+}
 
 test "analyze semantics call function import" {
     var arena = Arena.init(std.heap.page_allocator);
