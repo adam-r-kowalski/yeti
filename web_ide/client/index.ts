6c50b80e67433bb6534bf5c1010689830bdd4783
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-03-03 23:49:36 -0800

    ping pong client and server

--- web_ide/client/index.ts
+++ web_ide/client/index.ts
@@ -1,1 +1,9 @@
-console.log('hello adam')
+const ws = new WebSocket("ws://localhost:8080")
+
+ws.onopen = (event) => {
+        ws.send("ping")
+}
+
+ws.onmessage = (event) => {
+        console.log(event.data)
+}
--- web_ide/server/index.ts
+++ web_ide/server/index.ts
@@ -1,1 +1,10 @@
-console.log('server')
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+        ws.on('message', (data) => {
+                console.log('received: %s', data);
+                ws.send('pong');
+        });
+});
